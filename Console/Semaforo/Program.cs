53623c9c19f7abb128ec085193a69e98d551330b
Eduardo <eduzika2903@hotmail.com>
2020-11-12 20:30:06 -0300

    semáforo

---
+++ Console/Semaforo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Semaforo
+{
+    class Semaforo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O sinal está vermelho?");
+            bool red = bool.Parse(Console.ReadLine());
+
+
+            string consequencia = "";
+
+            if (red)
+            {
+                consequencia = "Pare o carro";
+            }else{
+                Console.WriteLine("O sinal está amarelo?");
+                bool yellow = bool.Parse(Console.ReadLine());
+                if(yellow){
+                    consequencia = "Reduza a velocidade e pare o carro";
+                }else{
+                    consequencia = "Continue andando";
+                }
+            }
+            Console.WriteLine(consequencia);
+        }
+    }
+}
