f5778b62827844f859e416d8242840d6fa7e7972
Eduardo <eduzika2903@hotmail.com>
2020-11-11 23:42:05 -0300

    Bem vindo

---
+++ Console/Projeto1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Bem vindo
+            Console.WriteLine("Digite seu nome: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Bem vindo: " + a);
+        }
+    }
+}
