98a0ab1689ae6eb77b921eb768e283a0eb463ff0
Eduardo <eduzika2903@hotmail.com>
2020-11-11 23:59:37 -0300

    calculadora

---
+++ Console/Projeto2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+        
+        Console.WriteLine("Digite o primeiro numero");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo numero");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int resultado = numero1 + numero2;
+
+        Console.WriteLine(numero1 + " + " + numero2 + " = " + resultado);
+        }
+    }
+}
