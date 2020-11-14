2b482ce932be420e094a089490b4b9c99dbfac59
Caue13087 <caue13087@gmail.com>
2020-11-14 20:22:28 -0300

    Exercicio 1 feito

---
+++ Console/Lições/idade em anos, meses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace idade_em_anos__meses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            int meses = idade * 12;
+            int dias = meses * 360;
+            int horas = dias *24;
+            int minutos = horas * 60;
+
+            Console.WriteLine("Voce ja viveu: " + meses + " meses, " + dias + " dias, " + horas + " horas, " + minutos + " minutos, ");
+        }
+    }
+}
