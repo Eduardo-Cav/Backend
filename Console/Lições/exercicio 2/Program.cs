84a609904e7492030b60115d8ef9227f350d0fb3
Caue13087 <caue13087@gmail.com>
2020-11-15 18:45:04 -0300

    Exercicio 2 Realizado

---
+++ Console/Lições/exercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano que você nasceu: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano atual: ");
+            int anoatual = int.Parse(Console.ReadLine());
+
+            int idade = anoatual - ano;
+
+            Console.WriteLine($"A sua idade é: {idade} anos");
+            
+            int semanas = 52;
+
+            int idadeSemana = idade * semanas;
+            Console.WriteLine($"A sua idade em semanas é de: {idadeSemana} semanas aproximadamente.");
+            
+        }
+    }
+} 
