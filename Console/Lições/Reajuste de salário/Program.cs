c1acc0bf750f5ca67ba4cdaffca4108cb5d8343f
Caue13087 <caue13087@gmail.com>
2020-11-15 18:47:16 -0300

    Reajuste de salário

---
+++ Console/Lições/Reajuste de salário/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reajuste_de_salário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a sua renda mensal?");
+            int salario = int.Parse(Console.ReadLine());
+            if(salario<500){
+                double aumento = salario*1.30;
+                Console.WriteLine("Seu salário será reajustado e você receberá um aumento de 30% em sua renda, que agora será de: " + aumento);
+            }else{
+                Console.WriteLine("Seu salário não condiz com os termos para receber um reajuste");
+            }
+        }   
+    
+    }
+
+}
