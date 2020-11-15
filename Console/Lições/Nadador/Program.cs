c11dcbd695122b37cce08da6c45afdea2de74c0c
Caue13087 <caue13087@gmail.com>
2020-11-15 18:50:01 -0300

    Exercício nadador

---
+++ Console/Lições/Nadador/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nadador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade>=5 && idade <=7){
+                Console.WriteLine("Você entrará para Categoria infantil A");
+            }else {
+                if(idade >=8 && idade <=10){
+                    Console.WriteLine("Você entrará para Categoria infantil B");
+                }else{
+                    if(idade >=11 && idade <= 13){
+                        Console.WriteLine("Você entrará para Categoria Juvenil A");
+                    }else{
+                        if(idade >=14 && idade <= 17){
+                            Console.WriteLine("Você entrará para Categoria Juvenil B");
+                        }else{
+                            Console.WriteLine("Você entrará para categoria Senior");
+                        }
+                    }    
+                }
+            }
+        }
+    }
+}
